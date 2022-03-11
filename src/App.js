6557a8539fb3962a11a7e28192d37ee674a706bd
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2022-03-12 00:03:10 +0300

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Actor from './pages/Actor'
 import SingleActor from './pages/SingleActor'
 import Movie from './pages/Movie'
+import Movies from "./pages/Movies";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -11,9 +13,11 @@ function App() {
       <Router>
         <Switch>
           <Route path='/actors/:actorId' component={SingleActor} />
+          <Route path="/movies" component={Movies} />
           <Route path='/movies/movie/:movieId' component={Movie} />
           <Route path='/actors' component={Actor} />
           <Route exact path='/' component={Home} />
+          <Route path="*" component={Error} />
         </Switch>
       </Router>
     </div>
